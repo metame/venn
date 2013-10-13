991753a60e0a364075e0c0533b98679e7ec66a51
Michael Erwin <mje@movewithmeta.com>
2013-10-13 13:52:48 -0400

    Dynamic menus assigned to each circle

--- play.js
+++ play.js
@@ -1,15 +1,29 @@
 var circle = '<div class="drag circle resize"><label id="label" for="circle" class="drag">Label</label></div>';
+var circlenav = '<nav class="circlemenu"><ul><li><a href="#">Label</a></li><li><a href="#">Delete</a></li><li><a href="#">Size</a></li><li><a href="#">Color</a></li></ul></nav>';
 
-$('document').ready(function()	{
+$(document).ready(function()	{
 	$('#add').click(function(e)	{
 		e.preventDefault();
 		$('#main').append(circle);
+
+		$('#menus').append(circlenav);
+
 		$('.circle').addClass(function( index ) {
   			return "circle-" + index;
-			});
+		});
+
+		$('.circlemenu').addClass(function( index )	{
+			return "cnav-" + index;
+		});
+
 		$('.drag').draggable();
 	});
+
+	$('.circle').hover(function()	{
+		$(this).fadeOut('fast');
+	});
 });
+
 
 /*need to build capability to select a circle to change options
 
