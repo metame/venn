3d4e01ac68c071873b562ec2a20eff595fd7ec10
Michael Erwin <mje@movewithmeta.com>
2013-12-20 00:04:33 -0500

    Replaced circle menus with context menus

--- play.js
+++ play.js
@@ -1,14 +1,11 @@
 var $label = '<label class="printedlabel drag">Label</label>';
 var circle = '<div class="drag circle">' + $label + '</div>';
-var circlenav = '<nav class="circlemenu"><ul><li class="label">Label</li><li>Delete</li><li>Size</li><li>Color</li></ul></nav>';
 
 /* basic venn functionality */
 $(document).ready(function()	{
 	$('#add').click(function(e)	{
 		e.preventDefault();
 		$('#main').append(circle); /* create circle */
-
-		$('#menus').append(circlenav); /* create circle menu */
 
 		/*draggable*/
 		$('.drag').draggable({containment: "document"});		
@@ -24,91 +21,40 @@ $(document).ready(function()	{
 		});
 
 		/* todo: add class based on index to group all elements belonging to a circle array in a single class */
+	/* label context menu */
+		$('.circle').contextMenu('circlemenu', {
+			'Rename': {
+				click: function(element){
+					var clabel = element.children(".printedlabel");
+					var relabel = prompt("enter label name");
 
-		
-		/* renaming Labels for circle elements */
-		var relabel = '<input class="relabel" type="text" placeholder="Label" name="Relabel">';
-		$('.label').one('click', function()	{
-			$('.circlemenu').prepend(relabel);
-
-			/* add input value to printed label */
-			$('.cnav-0 .relabel').change(function()	{
-				var val0 = $(this).val();
-				$('.circle-0 .printedlabel').html(val0);
-			});
-
-			$('.cnav-1 .relabel').change(function()	{
-				var val1 = $(this).val();
-				$('.circle-1 .printedlabel').html(val1);
-			});
-
-			$('.cnav-2 .relabel').change(function()	{
-				var val2 = $(this).val();
-				$('.circle-2 .printedlabel').html(val2);
-			});
-				/* currently have to add function for each circle and label. would like to bring this to a more simple, scalable solution */
-		});
-
-		/* delete menu function */
-		$(".cnav-0 ul li:contains('Delete')").click(function()	{
-			var array0 = $('.circle-0, .cnav-0');
-			$(array0).remove();
-		});
-
-		$(".cnav-1 ul li:contains('Delete')").click(function()	{
-			var array1 = $('.circle-1, .cnav-1');
-			$(array1).remove();
-		});
-
-		$(".cnav-2 ul li:contains('Delete')").click(function()	{
-			var array2 = $('.circle-2, .cnav-2');
-			$(array2).remove();
-		});
-		
-		/* resize menu function */
-		$(".cnav-0 ul li:contains('Size')").click(function()	{
-			$('.circle-0').addClass('resize');
-			$('.resize').resizable({
-				aspectRatio: true
-			});
-		});
-
-		$(".cnav-1 ul li:contains('Size')").click(function()	{
-			$('.circle-1').addClass('resize');
-			$('.resize').resizable({
-				aspectRatio: true
-			});
-		});
-
-		$(".cnav-2 ul li:contains('Size')").click(function()	{
-			$('.circle-2').addClass('resize');
-			$('.resize').resizable({
-				aspectRatio: true
-			});
-		});
-
-		/* color menu function */
-		/* bug in color menu: need to change input position so it doesn't fall in same place as relabel */
-		
-		$(".circlemenu ul li:contains('Color')").one('click', function()	{
-			var recolor = '<input class="recolor" type="text" placeholder="Standard, Hex, RGB, or RGBA Color (CSS format)" name="Recolor">';
-			$('.circlemenu').prepend(recolor);
-
-			$('.cnav-0 .recolor').change(function()	{
-				var color0 = $(this).val();
-				$('.circle-0').css('background-color',color0);
-			});
-
-			$('.cnav-1 .recolor').change(function()	{
-				var color1 = $(this).val();
-				$('.circle-1').css('background-color',color1);
-			});
-
-			$('.cnav-2 .recolor').change(function()	{
-				var color2 = $(this).val();
-				$('.circle-2').css('background-color',color2);
-			});
-		});
+					$(clabel).text(relabel);
+				}
+			},
+			'Delete': {
+				click: function(element){
+					element.remove();
+				}
+			},
+			'Size': {
+				click: function(element){
+					element.addClass('resize');
+					$('.resize').resizable({
+						aspectRatio: true
+					});
+				}
+			},
+			'Color': {
+				click: function(element){
+					var recolor = prompt("Enter Standard, Hex, RGB, or RGBA Color (CSS format)");
+					element.css('background-color',recolor);
+				}
+			}
+		},
+		{
+			leftClick: true
+		}
+		);	
 	});
 });
 
